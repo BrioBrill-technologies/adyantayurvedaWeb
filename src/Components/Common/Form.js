1ae76f374366f4b8a537a3985afd0749869c7c9a
Ruthvik L <48211227+ruthvikl@users.noreply.github.com>
2022-04-24 10:25:29 +0530

    material UI styling

--- src/Components/Common/Form.js
+++ src/Components/Common/Form.js
@@ -1,39 +1,77 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Button from './Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
 export default function BasicTextFields({ title, setPassword, setEmail, handleAction }) {
     return (
-        <div>
-            <div className="heading-container">
-                <h3>
-                    {title} Form
-                </h3>
-            </div>
-
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField
-                    id="email"
-                    label="Enter the Email"
-                    variant="outlined"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                    id="password"
-                    label="Enter the Password"
-                    variant="outlined"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+<ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {title}
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             </Box>
-
-            <Button title={title} handleAction={handleAction}/>
-        </div>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              title={title} handleAction={handleAction}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+            </Button>
+            </Box>
+        </Container>
+    </ThemeProvider>
     );
 }
