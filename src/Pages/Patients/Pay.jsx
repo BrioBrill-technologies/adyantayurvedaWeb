7e260b2d1a62727d3f491b291424ce06bcf10f85
Ruthvik L <48211227+ruthvikl@users.noreply.github.com>
2022-05-31 09:11:16 +0530

    Update Pay.jsx

--- src/Pages/Patients/Pay.jsx
+++ src/Pages/Patients/Pay.jsx
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getSingleApproved } from "../../Hooks/useFetch";
+import Footer from "../../Components/Navbar/Footer";
 
 function Pay(){
     const {state} = useLocation();
@@ -37,13 +38,13 @@ function Pay(){
         </div>
             {doctor && (
             <Grid display="flex" justifyContent="center" alignItems="center" sx={{marginTop:10}}>
-                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                <Grid item sx={3} md={{marginLeft:40}}>
                     <Card>
                         <CardContent>
                             <Typography>{state.place}</Typography>
                             <Typography>Includes Prescription</Typography>
                             <Divider />
-                            <Box display="flex" justifyContent="center" alignItems="center">
+                            <Box display="flex" >
                                 <Typography>
                                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M16.1999 6.75H1.7999V16.2H16.1999V6.75ZM16.1999 5.85H1.7999V2.7H16.1999V5.85ZM17.0999 2.7C17.0999 2.20294 16.697 1.8 16.1999 1.8H1.7999C1.30285 1.8 0.899902 2.20294 0.899902 2.7V16.2C0.899902 16.6971 1.30285 17.1 1.7999 17.1H16.1999C16.697 17.1 17.0999 16.6971 17.0999 16.2V2.7Z" fill="#3E3E3E"/>
@@ -51,7 +52,7 @@ function Pay(){
                                     </svg>
                                     On May 22, 2022
                                 </Typography>
-                                <Typography>
+                                <Typography style={{marginLeft:"auto"}}>
                                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M9 17.1C13.4735 17.1 17.1 13.4735 17.1 9C17.1 4.52649 13.4735 0.9 9 0.9C4.52649 0.9 0.9 4.52649 0.9 9C0.9 13.4735 4.52649 17.1 9 17.1ZM9 18C13.9706 18 18 13.9706 18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0 13.9706 4.02944 18 9 18Z" fill="#3E3E3E"/>
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M8.1 4.05H9V8.3636L13.3682 12.7318L12.7318 13.3682L8.1 8.7364V4.05Z" fill="#3E3E3E"/>
@@ -92,7 +93,10 @@ function Pay(){
                             Full Name*
                         </Typography>
                         <InputBase id="name" placeholder="Your Name"  />
-                        <InputBase id="name" placeholder="Your Name"  />
+                        <Typography>
+                            Your Mobile Number*
+                        </Typography>
+                        <InputBase id="mobile" placeholder="Mobile Number"  />
                         <Typography>
                             Bill Details
                         </Typography>
@@ -119,6 +123,7 @@ function Pay(){
                 </Grid>
             </Grid>
             )}
+            < Footer />
         </div>
     );
 }
